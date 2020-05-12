8b5c9020ca5c347e8c8ace73836b65a2b9346ada
Xavi <xavi@geekshubs.com>
2020-05-12 18:24:32 +0200

    [RED] Create test divisible by 3

--- fizzBuzz.ts
+++ fizzBuzz.ts
@@ -1,7 +1,7 @@
 export class FizzBuzz{
 
     public isFizz(value:number):boolean {
-        return (value === 3)? true: false;
+        return (value === 3) ? true: false;
     };
 
 }
--- suite.test.ts
+++ suite.test.ts
@@ -1,23 +1,40 @@
 import { FizzBuzz } from "./fizzBuzz";
 
-describe('FizzBuzz Testing', ()=>{
-    test('First  Test', function () {
+describe('FizzBuzz Testing', function (){
+    /*test('First  Test', function () {
         var expected =  true;
         var result = false;
         expect(result).toBe(expected);
-    })
+    })*/
 
-    test (' it Should Return Fizz If equals to Three',()=>{
+    test (' it Should Return Fizz If equals to Three',function (){
        
         //Arrange
         let _fizzBuzz = new FizzBuzz();
-        let value= 3;
+        const value= 3;
 
         //Act
-        var result  = _fizzBuzz.isFizz(value);
+        let result  = _fizzBuzz.isFizz(value);
         
         //Assertion
-        expect(result).toBeTruthy;
+        expect(result).toBeTruthy();
     
         })
+
+    test (' it Should Return Fizz If Divisible to Three',function(){
+    
+        //Arrange
+        let _fizzBuzz = new FizzBuzz();
+        const value= 6;
+
+        //Act
+        let result1  = _fizzBuzz.isFizz(value);
+        
+        //Assertion
+        expect(result1).toBeTruthy();
+    
+        })
+
+
+    
 });
